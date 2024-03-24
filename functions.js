2c7324818326378d604c5f25904b66cf5311cac3
milomadman <128404660+milomadman@users.noreply.github.com>
2024-03-24 22:04:23 +0100

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,13 @@
+/*addEventListener("mousemove", (e) => {
+    console.log("X: " + e.clientX);
+    console.log("Y: " + e.clientY);
+});*/
+
+function click() {
+    console.log("hi");
+    alert("You clicked the");
+}
+
+function showinfo() {
+    alert("give me modal");
+}
